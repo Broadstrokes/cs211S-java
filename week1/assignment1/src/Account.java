d8491190df84fde1da35a147b511e672ddb63430
Dan Sajjad <dan.sajjad@gmail.com>
2019-01-22 00:12:39 -0500

    Week 1: Add interface for an Account

--- week1/assignment1/src/Account.java
+++ week1/assignment1/src/Account.java
@@ -1,2 +1,6 @@
-public class Account {
+public interface Account {
+    public String getUserId();
+    public double getBalance();
+    public void deposit(double amount);
+    public void withdraw(double amount);
 }
