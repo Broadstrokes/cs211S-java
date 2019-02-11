91627f0ec0b1954dec38ddd0535d2a8a300d6a12
Dan Sajjad <dan.sajjad@gmail.com>
2019-02-10 20:35:03 -0800

    Week 4: Practice - add button to increment counter; use TilePane for layout

--- week4/src/M4Practice.java
+++ week4/src/M4Practice.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -12,31 +13,45 @@ import javafx.stage.Stage;
 
 public class M4Practice extends Application {
     // Control instance variables
-    private Button button;
+    private Button button1;
+    private Button button2;
     private Text helloText;
-
+    private Text countText;
+    private int count = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         // Group root = new Group();
 
-        // Pane is a way to organize - FlowPane organizes one after another
+        // Pane is a way to organize
+        // FlowPane organizes one after another
+        // TilePane laysout children in a grid of equally sized cells
         // Pane can serve as a root node
-        Pane pane = new FlowPane();
+
+
+        Pane tilePane = new TilePane();
 
         helloText = new Text("Hello world!!");
         helloText.setFont(Font.font("Times New Roman", 20));
         helloText.setFill(Color.RED);
-        pane.getChildren().add(helloText);
-
-        button = new Button("Click here");
-        button.setOnAction(this::handleButton);
-
-        pane.getChildren().add(button);
+        tilePane.getChildren().add(helloText);
 
 
-        Scene scene = new Scene(pane, 300, 300, Color.BEIGE);
+        button1 = new Button("Click here to change text");
+        button1.setOnAction(this::handleButton1);
+        tilePane.getChildren().add(button1);
+
+
+        countText = new Text(String.valueOf(this.count));
+        countText.setFill(Color.GREEN);
+        tilePane.getChildren().add(countText);
+
+        button2 = new Button("Click to increment counter");
+        button2.setOnAction(this::handleButton2);
+        tilePane.getChildren().add(button2);
+
+        Scene scene = new Scene(tilePane, 300, 300, Color.BEIGE);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Title");
@@ -44,8 +59,13 @@ public class M4Practice extends Application {
 
     }
 
-    private void handleButton(ActionEvent event) {
+    private void handleButton1(ActionEvent event) {
         helloText.setText("Hello universe!!!");
+    }
+
+    private void handleButton2(ActionEvent event) {
+        this.count++;
+        countText.setText(String.valueOf(count));
     }
 
     public static void main(String[] args) { launch(args); }
