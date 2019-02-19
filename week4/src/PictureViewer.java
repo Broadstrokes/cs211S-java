c6dc75191d81096c6ed13a9c07c791f29c96166d
Dan Sajjad <dan.sajjad@gmail.com>
2019-02-18 23:04:08 -0800

    Week 5: Add an image to the scene graph

--- week4/src/PictureViewer.java
+++ week4/src/PictureViewer.java
@@ -1,7 +1,12 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class PictureViewer extends Application {
@@ -10,12 +15,19 @@ public class PictureViewer extends Application {
     public void start(Stage primaryStage) {
         // Pane
         VBox primaryBox = new VBox();
+        // Get image
+        Image beeImage = new Image("bee.jpg");
+        // Provide image to ImageView class
+        ImageView beeImageView = new ImageView(beeImage);
+        // Add ImageView instance to the pane instance
+        primaryBox.getChildren().add(beeImageView);
+
 
         // Scene using the pane as root node
-        Scene scene = new Scene(primaryBox, 500, 500, Color.LIGHTGRAY);
+        Scene scene = new Scene(primaryBox, 500, 400, Color.LIGHTGRAY);
 
         // Set title for the primaryStage
-        primaryStage.setTitle("Batman");
+        primaryStage.setTitle("Bee");
         // Set scene on the primaryStage
         primaryStage.setScene(scene);
         // Show primaryStage
