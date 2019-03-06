476385f199929792b140da3fc622138a9b7f4e38
Dan Sajjad <dan.sajjad@gmail.com>
2019-03-06 02:57:09 -0800

    M7 - Generate queries for the eviction notice data

--- week7/src/ListTester.java
+++ week7/src/ListTester.java
@@ -1,10 +1,9 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
+@SuppressWarnings("Duplicates")
 public class ListTester {
     public static void main(String[] args) {
         List<EvictionNotice> evictionNoticeList = generateListFromFile();
@@ -12,10 +11,17 @@ public class ListTester {
         println("Number of eviction records in list: " + evictionNoticeList.size());
     }
 
-    private static List<EvictionNotice> generateListFromFile() {
-        List<EvictionNotice> evictionNoticeList = new ArrayList<EvictionNotice>();
 
-        try(Scanner fileScan = new Scanner(new FileReader(new File("resources/data1.csv")))) {
+    /**
+     * Creates a list of eviction notices from a csv file
+     *
+     * @param filePath path of the cvs file
+     * @return ArrayList holding EvictionNotice objects
+     */
+    private static List<EvictionNotice> generateListFromFile(String filePath) {
+        List<EvictionNotice> evictionNoticeList = new ArrayList<>();
+
+        try(Scanner fileScan = new Scanner(new FileReader(new File(filePath)))) {
             String line = fileScan.nextLine(); // read column headers
 
 
@@ -31,13 +37,11 @@ public class ListTester {
                     boolean illegalUse = Boolean.valueOf(isBlank(lineArray[3])); // if item is NA - boolean is false
                     String neighborhood = isBlank(lineArray[4]);
 
-                    println(id + " | " +  zip + " | " + fileDate + " | " + illegalUse + " | " +  neighborhood);
-
                     EvictionNotice evictionNotice = new EvictionNotice(id, zip, neighborhood, illegalUse, fileDate);
                     evictionNoticeList.add(evictionNotice);
                 }
             } catch (Exception e) {
-                println(">>>> Couldnt parse data ");
+                println(">>>> Couldn't parse data ");
                 e.printStackTrace();
             }
 
@@ -47,6 +51,69 @@ public class ListTester {
         }
 
         return evictionNoticeList;
+    }
+
+
+    /**
+     * Generate a map of count of eviction notices by ZipCode
+     * @param list  list containing EvictionNotice objects
+     * @return a map of count of eviction notices by ZipCode
+     */
+    public static Map<String, Integer> generateMapOfEvictionCountsByZipCode(List<EvictionNotice> list) {
+        Map<String, Integer> map = new HashMap<>();
+        Iterator<EvictionNotice> itr = list.iterator();
+
+        while(itr.hasNext()) {
+            EvictionNotice notice = itr.next();
+            String zipcode = notice.getZip();
+
+            map.put(zipcode, map.getOrDefault(zipcode, 0) + 1);
+        }
+
+        return map;
+    }
+
+    /**
+     * Generate a map of count of eviction notices by neighborhood
+     * @param list list containing EvictionNotice objects
+     * @return a map of count of eviction notices by neighborhood
+     */
+    public static Map<String, Integer> generateMapOfEvictionCountsByNeighborhood(List<EvictionNotice> list) {
+        Map<String, Integer> map = new TreeMap<>();
+        Iterator<EvictionNotice> itr = list.iterator();
+
+        while(itr.hasNext()) {
+            EvictionNotice notice = itr.next();
+            String neighborhood = notice.getNeighborhood();
+
+            map.put(neighborhood, map.getOrDefault(neighborhood, 0) + 1);
+        }
+
+        return map;
+    }
+
+    /**
+     * Display in console the neighborhood with the most eviction notices
+     * @param map map of eviction notice counts by neighborhood
+     */
+    public static void printNeighborhoodWithMostEvictionNotices(Map<String, Integer> map) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        String neighborhood = "";
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            int noticeCount = entry.getValue();
+            if (noticeCount > max) {
+                max = noticeCount;
+                neighborhood = entry.getKey();
+            }
+            if (noticeCount < min) {
+                min = noticeCount;
+            }
+        }
+
+        print(neighborhood + ": " + max);
+        println(" | Range for notice count: [" + min + ", " + max + "]");
     }
 
     /*
