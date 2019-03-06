6bbcad58ddd298517c4fcbaff9217d853e21430a
Dan Sajjad <dan.sajjad@gmail.com>
2019-03-06 00:59:33 -0800

    M7 - Add functionality to read a csv file and handle missing fields

--- week7/src/ListTester.java
+++ week7/src/ListTester.java
@@ -1,5 +1,10 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class ListTester {
     public static void main(String[] args) {
@@ -8,13 +13,47 @@ public class ListTester {
 
     private static List<EvictionNotice> generateListFromFile() {
         List<EvictionNotice> evictionNoticeList = new ArrayList<EvictionNotice>();
-        // WRITE LOGIC
+
+        try(Scanner fileScan = new Scanner(new FileReader(new File("resources/data2.csv")))) {
+            String line = fileScan.nextLine(); // read column headers
+
+
+            try {
+                while (fileScan.hasNextLine()) {
+                    line = fileScan.nextLine();
+                    String[] lineArray = line.split(",", -1); // pass in -1 to capture railing empty strings
+
+                    String id = isBlank(lineArray[0]);
+                    String zip = isBlank(lineArray[1]);
+                    String fileDate = isBlank(lineArray[2]);
+                    String illegalUse = isBlank(lineArray[3]);
+                    String neighborhood = isBlank(lineArray[4]);
+
+                    println(id + " | " +  zip + " | " + fileDate + " | " + illegalUse + " | " +  neighborhood);
+                }
+            } catch (Exception e) {
+                println(">>>> Couldnt parse data ");
+                e.printStackTrace();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return evictionNoticeList;
     }
 
     /*
         HELPER FUNCTIONS
      */
+
+
+
+    public static String isBlank(String str) {
+        if (str == null || (str.length() == 0)) { str = "NA"; }
+        return str;
+    }
 
 
     public static void println(Object line) { System.out.println(line); }
