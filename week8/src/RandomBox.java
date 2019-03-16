0c5ce56d8c5569680b9ed5e89b9d41dabc9b9044
Dan Sajjad <dan.sajjad@gmail.com>
2019-03-16 02:18:36 -0700

    Week 8 - Add methods size & getListOfRandomItems

--- week8/src/RandomBox.java
+++ week8/src/RandomBox.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class RandomBox <T> {
     List<T> listOfRandomItems;
@@ -21,19 +19,51 @@ public class RandomBox <T> {
         Member Methods
      */
 
+    /**
+     * Adds an item of type T to the RandomBox
+     * @param newItem items to add to the box; can contain duplicates
+     */
     public void addItem(T newItem) {
         listOfRandomItems.add(newItem);
     }
 
+    /**
+     * Select & return a random item of type T from the RandomBox list
+     * @return return a random item of type T from the RandomBox list or null if list is empty
+     */
     public T drawWinner() {
         Random random = new Random();
-        return listOfRandomItems.get(random.nextInt(listOfRandomItems.size()));
+
+        if (listOfRandomItems.size() > 0) {
+            return listOfRandomItems.get(random.nextInt(listOfRandomItems.size()));
+        }
+
+        return null;
     }
 
+    /**
+     * Displays all the entries in the list
+     */
     public void displayEntries() {
         System.out.println("===== Displaying entries =====");
         listOfRandomItems.forEach(item -> System.out.println(item));
         System.out.println("===== End of entries =====");
+    }
+
+    /**
+     * Return the number of items in the list
+     * @return Return the number of items in the list
+     */
+    public int size () {
+        return listOfRandomItems.size();
+    }
+
+    /**
+     * Retrieve the list of items
+     * @return list containing all the items
+     */
+    public List<T> getListOfRandomItems() {
+        return listOfRandomItems;
     }
 
     /*
