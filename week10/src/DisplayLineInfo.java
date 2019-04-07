06cf0a19d90c152478f8f02705db52d83a9b26be
Dan Sajjad <dan.sajjad@gmail.com>
2019-04-07 14:48:39 -0700

    M10 - Add check to handle if line is null

--- week10/src/DisplayLineInfo.java
+++ week10/src/DisplayLineInfo.java
@@ -11,45 +11,68 @@ public interface DisplayLineInfo {
     public static DisplayLineInfo createDisplayLineInfo(InfoType type) {
         switch (type) {
             case DISTANCE: return (Line line) -> {
-                double x1 = line.getStartX();
-                double y1 = line.getStartY();
-                double x2 = line.getEndX();
-                double y2 = line.getEndY();
+                String output = "";
 
-                double distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+                if (line != null) {
+                    double x1 = line.getStartX();
+                    double y1 = line.getStartY();
+                    double x2 = line.getEndX();
+                    double y2 = line.getEndY();
 
-                String output = String.format("%.1f", distance);
+                    double distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+
+                    output = String.format("%.1f", distance);
+
+                }
 
                 return output;
             };
             case MIDPOINT: return (Line line) -> {
-                double x1 = line.getStartX();
-                double x2 = line.getEndX();
-                double y1 = line.getStartY();
-                double y2 = line.getEndY();
-                return "(" + (x1 + x2) / 2 + ", " + (y1 + y2) / 2 + ")";
+                String output = "";
+
+                if (line != null) {
+                    double x1 = line.getStartX();
+                    double x2 = line.getEndX();
+                    double y1 = line.getStartY();
+                    double y2 = line.getEndY();
+                    output = "(" + (x1 + x2) / 2 + ", " + (y1 + y2) / 2 + ")";
+                }
+
+                return output;
             };
             case VERTHORZ: return (Line line) -> {
-                double x1 = line.getStartX();
-                double x2 = line.getEndX();
-                double y1 = line.getStartY();
-                double y2 = line.getEndY();
+                String output = "";
 
-                boolean isVertical  = (x1 == x2);
-                boolean isHorizontal = (y1 == y2);
+                if (line != null) {
 
-                return "Vertical: " + isVertical +  " | Horizontal: " + isHorizontal;
+                    double x1 = line.getStartX();
+                    double x2 = line.getEndX();
+                    double y1 = line.getStartY();
+                    double y2 = line.getEndY();
+
+                    boolean isVertical = (x1 == x2);
+                    boolean isHorizontal = (y1 == y2);
+
+                    output = "Vertical: " + isVertical + " | Horizontal: " + isHorizontal;
+                }
+
+                return output;
             };
 
             case IS_A_LINE: return (Line line) -> {
-                double x1 = line.getStartX();
-                double x2 = line.getEndX();
-                double y1 = line.getStartY();
-                double y2 = line.getEndY();
+                String output = "";
 
-                boolean isALine  = (x1 != x2) && (y1 != y2);
+                if (line != null) {
+                    double x1 = line.getStartX();
+                    double x2 = line.getEndX();
+                    double y1 = line.getStartY();
+                    double y2 = line.getEndY();
 
-                return Boolean.toString(isALine);
+                    boolean isALine = (x1 != x2) && (y1 != y2);
+
+                    output = Boolean.toString(isALine);
+                }
+                return output;
             };
         }
 
