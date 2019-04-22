dee87fd050ff167abbf73c32d3f50a5b02b08c14
Dan Sajjad <dan.sajjad@gmail.com>
2019-04-22 16:24:23 -0400

    M11.2: Complete part 2

--- week11Part2/src/HomeworkM11PartTwo.java
+++ week11Part2/src/HomeworkM11PartTwo.java
@@ -1,7 +1,6 @@
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.*;
 import java.io.*;
+import java.util.stream.Collectors;
 
 public class HomeworkM11PartTwo {
 
@@ -15,21 +14,35 @@ public class HomeworkM11PartTwo {
         // QUESTION 1: How many total chemicals appear across all products? 
         // Example: Product1 contains Chemical1, Chemical2 and Product2 contains Chemical2 and Chemical3
         //          count would be 4 total chemicals
-        int numberChemicalsInAllProducts = 0; 
+        int numberChemicalsInAllProducts = productList
+                .stream()
+                .mapToInt(Product::getNumberOfChemicals)
+                .sum();
+
         System.out.println("Number of chemicals in all products \t\t\t81791: " + numberChemicalsInAllProducts);
      
         // QUESTION 2: How many different companies are in the dataset?
-        int numberCompanies = 0;
+        int numberCompanies = productList
+                .stream()
+                .map(Product::getCompany)
+                .collect(Collectors.toSet())
+                .size();
         System.out.println("Number of companies \t\t\t\t\t549: " + numberCompanies);
         
         // QUESTION 3: How many products have 4 or more chemicals?
-        long numberProductsFourOrMore = 0;
+        long numberProductsFourOrMore = productList
+                .stream()
+                .filter(product -> product.getNumberOfChemicals() > 3)
+                .count();
         System.out.println("Number of products with 4 or more chemicals \t\t193 :" + numberProductsFourOrMore);
 
         //  QUESTION 4: Create a Map with key = company name and value = list of products for that company.
         // Hint: use Collectors.groupingBy!
-        Map<String, List<Product>> companyProductMap = null;
-        
+        Map<String, List<Product>> companyProductMap = productList
+                .stream()
+//                .collect(Collectors.toMap(Product::getCompany, Product::getChemicals));
+                .collect(Collectors.groupingBy(product -> product.getCompany()));
+
         // checks that the map is correct; consider adding additional checks!
         System.out.println("Number of companies (keys) \t\t\t\t549: " + companyProductMap.size());
         System.out.println("Number of products for Aloette Cosmetics Inc. \t\t77: " + (companyProductMap.get("Aloette Cosmetics Inc.")).size());
@@ -39,23 +52,32 @@ public class HomeworkM11PartTwo {
 
         // QUESTION 5: Use the map you created above. Which company has the most products?
         // Hint: Use .max(Comparator) and define the Comparator to compare companies based on size of their lists.
-        String companyMostProducts = "";
+        String companyMostProducts = companyProductMap
+                .keySet()
+                .stream()
+                .max((a,b) -> Integer.compare(companyProductMap.get(a).size(), companyProductMap.get(b).size()))
+                .get();
+
         System.out.println("Company with most products \t\t\t\tLOreal USA: " + companyMostProducts);
-        
-       // Use this map for the next questions. 
+
+       // Use this map for the next questions.
        // This is a map with key = chemical name and value = list of products that contain that chemical.
        // This code creates a map with all chemicals and empty lists.
        Map<String, List<Product>> chemicalProductMap = new HashMap<String, List<Product>>();
        productList.stream().forEach(
                product -> {  product.getChemicals().stream().forEach(
                        chemicalName -> chemicalProductMap.putIfAbsent(chemicalName, new ArrayList<Product>()));
-                          }      
+                          }
                );
-   
-       
+
+
        // QUESTION 6: Fill the lists (the value) of the map above.
        // Hint: Use a nested stream (one stream of productList and then a separate stream for each list of each product).
-       // YOUR CODE HERE- NO RETURN VALUE
+        productList.stream()
+                .forEach(product -> product.getChemicals()
+                        .stream()
+                        .forEach(chemical -> chemicalProductMap.get(chemical).add(product))
+                );
 
        // checks that the map is correct; consider adding additional checks!
        System.out.println("Number of products that contain Formaldehyde (gas) \t\t121: " + (chemicalProductMap.get("Formaldehyde (gas)")).size());
@@ -64,7 +86,11 @@ public class HomeworkM11PartTwo {
 
        // QUESTION 7: Which chemical appears in the most products?
        // Hint: use max(Comparator) again. Define your comparator to compare chemical names based on the size of the list of products.
-       String mostOccurringChemical = "";
+       String mostOccurringChemical = chemicalProductMap
+               .keySet()
+               .stream()
+               .max((a, b) -> Integer.compare(chemicalProductMap.get(a).size(), chemicalProductMap.get(b).size()))
+               .get();
        System.out.println("Most common chemical \t\t\t\t\t\tTitanium dioxide: " + mostOccurringChemical);
        System.out.println("It appears in this many products \t\t\t\t67898: " + (chemicalProductMap.get("Titanium dioxide")).size());
 
@@ -73,7 +99,8 @@ public class HomeworkM11PartTwo {
 
     private static List<Product> createList() {
         String line = "";
-        String fileName = "ChemicalData.csv";
+        String fileName = "resources/ChemicalData.csv";
+//        String fileName = "resources/sample.csv";
         List<Product> list = new ArrayList<Product>();
 
         try (Scanner fileScan = new Scanner(
