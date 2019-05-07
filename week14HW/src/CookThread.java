c9040054340f03f68e95370052329b4437ec9ca4
Dan Sajjad <dan.sajjad@gmail.com>
2019-05-06 22:56:24 -0700

    Week 14 HW - Complete CookThread

--- week14HW/src/CookThread.java
+++ week14HW/src/CookThread.java
@@ -1,20 +1,29 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
 
 public class CookThread implements Runnable {
     List<Food> foodList;
 
-    public CookThread(List<Food> foodList) {
+    private BlockingQueue<Food> queue;
+
+
+    public CookThread(List<Food> foodList, BlockingQueue queue) {
         this.foodList = foodList;
+        this.queue = queue;
     }
 
     @Override
     public void run() {
-
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
+        while(!foodList.isEmpty()) {
+            try {
+                Food order = foodList.remove(0);
+                System.out.println("COOK READY");
+                System.out.println("COOK STARTING: " + order.toString());
+                Thread.sleep(order.getCookTime() * 1000);
+                queue.put(order);
+                System.out.println("COOK ENDING: " + order.toString());
+            } catch (InterruptedException ex) { }
+        }
     }
 }
