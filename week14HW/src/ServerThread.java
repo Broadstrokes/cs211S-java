6284854bf61bfcb68e5b173502877af1259b452a
Dan Sajjad <dan.sajjad@gmail.com>
2019-05-06 22:56:36 -0700

    Week 14 HW - Complete ServerThread

--- week14HW/src/ServerThread.java
+++ week14HW/src/ServerThread.java
@@ -1,12 +1,25 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ServerThread implements Runnable {
+    private int numServed = 0;
+    private BlockingQueue<Food> queue;
 
-    @Override
-    public void run() {
-
+    public ServerThread(BlockingQueue<Food> queue) {
+        this.queue = queue;
     }
 
     @Override
-    public String toString() {
-        return super.toString();
+    public void run() {
+        while (numServed < 8) {
+            try {
+                System.out.println("SERVER READY");
+                Food order = queue.take();
+                System.out.println("SERVER STARTING: " + order.toString());
+                Thread.sleep(order.getServeTime() * 1000);
+                System.out.println("SERVER ENDING: " + order.toString());
+                numServed++;
+            } catch (InterruptedException ex) {}
+        }
     }
 }
